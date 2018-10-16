f4e18c799e5c08b074453b7af6429d3c4f7d0a2e
Adam Dayan <ad6617@edge27.doc.ic.ac.uk>
2018-10-16 10:35:12 +0100

    added the make move function

--- sudoku.cpp
+++ sudoku.cpp
@@ -84,4 +84,54 @@ bool is_complete(char board[9][9])
   return true;
 }
 
+/* function to check whether a digit already exists in a given column. returns boolean */
+bool check_column(char digit, int column, char board[9][9])
+{
+  for (int row = 0; row < 9; row++)
+    {
+      if (board[row][column] == digit)
+	return false;
+    }
+
+  return true;
+}
+
+/* function to check whether a digit already exists in a given row. returns boolean */
+bool check_row(char digit, int row, char board[9][9])
+{
+  for (int column = 0; column < 9; column++)
+    {
+      if (board[row][column] == digit)
+	return false;
+    }
+
+  return true;
+}
+
+/* function to check whether given coordinates are in bounds */
+bool in_bounds(int row, int column)
+{
+  if ((row >= 0 && row < 9) && (column >= 0 && column < 9))
+    return true;
+  else
+    return false;
+}
+
+/* function to check whether a move is legal; if so it will write that move to the board */ 
+bool make_move(char position[3], char digit, char board[9][9])
+{
+  int row, col;
+
+  row = position[0] - 'A';
+  col = position[1] - 49; //-48 changes char to digit; -1 changes it to an index
+
+  if (check_column(digit, col, board) && check_row(digit, row, board) && in_bounds(row, col))
+    {
+      board[row][col] = digit;
+      return true;
+    }
+  else
+    return false;
+}
+  
   
--- sudoku.h
+++ sudoku.h
@@ -7,4 +7,16 @@ void display_board(const char board[9][9]);
 /* Function that checks whether all fields on a sudoku board are occupied by digits */ 
 bool is_complete(char board[9][9]) ; 
 
+/* function to check whether a digit already exists in a given column. returns boolean */
+bool check_column(char digit, int column, char board[9][9]);
+
+/* function to check whether a digit already exists in a given row. returns boolean */
+bool check_row(char digit, int row, char board[9][9]);
+
+/* function to check whether given coordinates are in bounds */
+bool in_bounds(int row, int column);
+
+/* function to check whether a move is legal; if so it will write that move to the board */ 
+bool make_move(char position[2], char digit, char board[9][9]); 
+
 #endif
